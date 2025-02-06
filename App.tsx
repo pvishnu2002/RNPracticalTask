c2b7a9ec73481d120d5630a715b23edba8c42fd4
pvishnu2002 <106003747+pvishnu2002@users.noreply.github.com>
2025-02-06 13:45:01 +0530

    remove commented code

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import { View } from 'react-native';
 import React from 'react';
 import HomeScreen from './src/screen/HomeScreen';
-import Demo from './src/screen/Demo';
 
 const App = () => {
   return (
     <View style={{flex:1}}>
       <HomeScreen />
-      {/* <Demo /> */}
     </View>
   );
 };
